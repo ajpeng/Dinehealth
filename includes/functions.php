6a56c3772772be99addf5fe1cbd54a62e89fa147
Alex Peng <jjpeng@ryerson.ca>
2017-06-04 09:43:30 -0400

    added README

--- includes/functions.php
+++ includes/functions.php
@@ -4,4 +4,22 @@
 			die("Database query failed.");
 		}
 	}
+
+	function implodeAllData(){
+		if(isset($_POST["ESTABLISHMENT_NAME"])){
+            global $connection;
+            $ESTABLISHMENT_NAME = $_POST["ESTABLISHMENT_NAME"];
+            $safe_estb_name_id = mysqli_real_escape_string($connection, $ESTABLISHMENT_NAME);
+            $query = "SELECT * FROM Dinesafe ";
+            $query .= "WHERE ESTABLISHMENT_NAME = ";
+            $query .= "'{$safe_estb_name_id}'";
+            //$query .= "LIMIT 1";
+            $dine_set = mysqli_query($connection ,$query);
+            confirm_query($dine_set);
+            while ($dine = mysqli_fetch_assoc($dine_set)){
+            	$result = implode(" | " ,$dine);
+            	$result .= "<br/>";
+            	$result .= "<br/>";
+            return $result;
+	}
 ?>
